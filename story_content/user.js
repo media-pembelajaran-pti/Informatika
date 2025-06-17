58c2178708d60e16394a66ad2a8f847e8134f90d
kiky <lilfakh0156@gmail.com>
2025-06-18 03:21:27 +0800

    DSI v0.10
    Update

---
+++ story_content/user.js
@@ -0,0 +1,199 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6frZCNq8fz8":
+        Script1();
+        break;
+      case "6aj6l5cXlUz":
+        Script2();
+        break;
+      case "5lzDLC2ch5Y":
+        Script3();
+        break;
+      case "6Cr0vA4wyTh":
+        Script4();
+        break;
+      case "6BvK9JZJlBQ":
+        Script5();
+        break;
+      case "5sNZvEfmQFm":
+        Script6();
+        break;
+      case "6eOSXP5IFt0":
+        Script7();
+        break;
+      case "6gd7dQT4Nvp":
+        Script8();
+        break;
+      case "687SulNvvhD":
+        Script9();
+        break;
+      case "6EKTqEbezwB":
+        Script10();
+        break;
+      case "60Fx9QpNT8T":
+        Script11();
+        break;
+      case "6lwiH43qmAu":
+        Script12();
+        break;
+      case "65J6VKpxC2j":
+        Script13();
+        break;
+      case "6hltzyqKMmQ":
+        Script14();
+        break;
+      case "5WsBt5BN1Ty":
+        Script15();
+        break;
+      case "6NtByYdOlsN":
+        Script16();
+        break;
+      case "5xbZ8KtE7zf":
+        Script17();
+        break;
+  }
+}
+
+function Script1()
+{
+  if (!window.bgMusic) {
+    window.bgMusic = new Audio("story_content/lagu1.mp3");
+    window.bgMusic.loop = true;
+}
+window.bgMusic.play();
+
+}
+
+function Script2()
+{
+  if (!window.bgMusic) {
+    window.bgMusic = new Audio("story_content/lagu1.mp3");
+    window.bgMusic.loop = true;
+}
+window.bgMusic.play();
+
+}
+
+function Script3()
+{
+  if (window.bgMusic && !window.bgMusic.paused) {
+    window.bgMusic.pause(); // hanya pause, tidak reset
+}
+}
+
+function Script4()
+{
+  if (!window.bgMusic) {
+    window.bgMusic = new Audio("story_content/lagu1.mp3");
+    window.bgMusic.loop = true;
+}
+window.bgMusic.play();
+
+}
+
+function Script5()
+{
+  if (window.bgMusic && !window.bgMusic.paused) {
+    window.bgMusic.pause(); // hanya pause, tidak reset
+}
+}
+
+function Script6()
+{
+  if (window.bgMusic && !window.bgMusic.paused) {
+    window.bgMusic.pause(); // hanya pause, tidak reset
+}
+}
+
+function Script7()
+{
+  if (!window.bgMusic) {
+    window.bgMusic = new Audio("story_content/lagu1.mp3");
+    window.bgMusic.loop = true;
+}
+window.bgMusic.play();
+
+}
+
+function Script8()
+{
+  if (window.bgMusic && !window.bgMusic.paused) {
+    window.bgMusic.pause(); // hanya pause, tidak reset
+}
+}
+
+function Script9()
+{
+  if (window.bgMusic && !window.bgMusic.paused) {
+    window.bgMusic.pause(); // hanya pause, tidak reset
+}
+}
+
+function Script10()
+{
+  if (!window.bgMusic) {
+    window.bgMusic = new Audio("story_content/lagu1.mp3");
+    window.bgMusic.loop = true;
+}
+window.bgMusic.play();
+
+}
+
+function Script11()
+{
+  if (window.bgMusic && !window.bgMusic.paused) {
+    window.bgMusic.pause(); // hanya pause, tidak reset
+}
+}
+
+function Script12()
+{
+  if (window.bgMusic && !window.bgMusic.paused) {
+    window.bgMusic.pause(); // hanya pause, tidak reset
+}
+}
+
+function Script13()
+{
+  if (!window.bgMusic) {
+    window.bgMusic = new Audio("story_content/lagu1.mp3");
+    window.bgMusic.loop = true;
+}
+window.bgMusic.play();
+
+}
+
+function Script14()
+{
+  if (window.bgMusic && !window.bgMusic.paused) {
+    window.bgMusic.pause(); // hanya pause, tidak reset
+}
+}
+
+function Script15()
+{
+  if (window.bgMusic && !window.bgMusic.paused) {
+    window.bgMusic.pause(); // hanya pause, tidak reset
+}
+}
+
+function Script16()
+{
+  if (window.bgMusic && !window.bgMusic.paused) {
+    window.bgMusic.pause(); // hanya pause, tidak reset
+}
+
+}
+
+function Script17()
+{
+  if (!window.bgMusic) {
+    window.bgMusic = new Audio("story_content/lagu1.mp3");
+    window.bgMusic.loop = true; // atau false jika tidak ingin looping
+}
+window.bgMusic.play();
+
+}
+
